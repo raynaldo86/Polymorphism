362069fdab644c327df98a4932d91cd84cfcdb78
raynaldo86 <raynaldo.3003@students.amikom.ac.id>
2020-05-06 11:56:44 +0700

    Tugas Polymorphism

---
+++ LaserJet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class LaserJet : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("LaserJer display dimension : 12*12");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("LaserJet printer printing....");
+        }
+    }
+}
---
+++ PrinterWindows.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class PrinterWindows
+    {
+        public string name { get; set; }
+
+        public virtual void Show()
+        {
+            Console.WriteLine("Pilih Printer");
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine("Pilih Printer");
+        }
+    }
+}
